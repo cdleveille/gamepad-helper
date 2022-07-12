414fb91708931ac380f2b6bbe0ff39fafa127e4f
Chris Leveille <cdleveille@gmail.com>
2022-07-12 14:45:06 -0400

    refactoring and fixes

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
+# gamepad-helper
+
+Lightweight wrapper class for the HTML5 Gamepad API.
+
 ```bash
-$ npm install gamepad-helper
+$ npm i gamepad-helper
 
 $ yarn add gamepad-helper
 ```
 
-The `GamepadHelper` class imported as a module into existing code and used by calling the static `GamepadHelper.update()` method within the game loop. Supports up to 4 gamepads connected simultaneously.
+The `GamepadHelper` class can be imported and used by calling the `GamepadHelper.update()` static method within your game loop.
 
 ```javascript
 import GamepadHelper from "gamepad-helper";
 
-// call inside game loop
+// ...within game loop
 GamepadHelper.update();
 ```
 
-When a button on a connected gamepad is pressed or released, a custom event will be dispatched to the document containing a `detail` property identifying the gamepad/button that was pressed/released.
+When a button on a connected gamepad is pressed or released, a `CustomEvent` will be dispatched to the document containing a `detail` property identifying the gamepad/button that was pressed/released.
 
-There are two custom event types:
+There are two `CustomEvent` types:
 
 -   `gamepadbuttondown` - dispatched when the `value` property of any button changes from `0` to `1`
 -   `gamepadbuttonup` - dispatched when the `value` property of any button changes from `1` to `0`
@@ -23,36 +27,58 @@ There are two custom event types:
 Here is an example that simply logs the gamepad/button that was pressed down:
 
 ```javascript
+// JavaScript
 document.addEventListener("gamepadbuttondown", (event) => {
-	console.log(
-		"button " +
-			event.detail.button +
-			" on gamepad " +
-			event.detail.gamepad +
-			" was pressed down"
-	);
+	const { button, gamepad } = event.detail;
+	console.log(`button ${button} on gamepad ${gamepad} was pressed down`);
+});
+
+// TypeScript
+import type { IGamepadButtonEventDetail } from "gamepad-helper";
+document.addEventListener("gamepadbuttondown", (event: CustomEvent<IGamepadButtonEventDetail>) => {
+	const { button, gamepad } = event.detail;
+	console.log(`button ${button} on gamepad ${gamepad} was pressed down`);
 });
 ```
 
 Here is an example which causes player 1 to jump if button 0 on gamepad 0 is released:
 
 ```javascript
+// JavaScript
 document.addEventListener("gamepadbuttonup", (event) => {
-	if (event.detail.gamepad == 0 && event.detail.button == 0) {
+	const { button, gamepad } = event.detail;
+	if (gamepad === 0 && button === 0) {
+		player1.jump();
+	}
+});
+
+// TypeScript
+import type { IGamepadButtonEventDetail } from "gamepad-helper";
+document.addEventListener("gamepadbuttonup", (event: CustomEvent<IGamepadButtonEventDetail>) => {
+	const { button, gamepad } = event.detail;
+	if (gamepad === 0 && button === 0) {
 		player1.jump();
 	}
 });
 ```
 
-The instantaneous state of any button or axis on a connected gamepad can also be queried via the `getButtonValue()` and `getAxisValue()` methods.
+The instantaneous state of any button or axis on a connected gamepad can also be queried via the `GamepadHelper.getButtonValue()` and `GamepadHelper.getAxisValue()` static methods.
 
-The `mode` property of an instance of the `GamepadHelper` class can be set to `default` (numeric labels), `xbox`, or `playstation` to change the button labels if desired:
+To log informational output to the console, set `GamepadHelper.logOutput` to `true` (`false` by default):
 
 ```javascript
-GamepadHelper.setMode("default");
-GamepadHelper.setMode("xbox");
-GamepadHelper.setMode("playstation");
+GamepadHelper.logOutput = true;
 ```
+
+```text
+gamepad 0 connected
+gamepad 0: button 0 is down
+gamepad 0: button 0 is up
+```
+
+Check it out on npm:
+
+-   https://www.npmjs.com/package/gamepad-helper
 
 More info on the Gamepad API:
 
--- src/index.ts
+++ src/index.ts
@@ -1,174 +1,88 @@
 export type GamepadHub = { [key: number]: Gamepad };
 
-export type GamepadMode = "xbox" | "playstation" | "default";
-
-export interface IButtonEvent {
-	detail: {
-		gamepad: number;
-		button: number | string;
-	};
+export interface IGamepadButtonEventDetail {
+	gamepad: number;
+	button: number;
 }
 
 export default class GamepadHelper {
-	static gamepads: GamepadHub = {};
-	static gamepadsLastUpdate: GamepadHub;
-	static mode: GamepadMode = "default";
-	static buttonDownEvent: CustomEvent<{ gamepad: number; button: string | number }>;
-	static buttonUpEvent: CustomEvent<{ gamepad: number; button: string | number }>;
+	public static gamepads: GamepadHub = {};
+	public static gamepadsLastUpdate: GamepadHub;
+	public static logOutput = false;
 
-	static createButtonDownEvent(gamepad: number, button: number | string) {
-		GamepadHelper.buttonDownEvent = new CustomEvent("gampadbuttondown", {
-			detail: { gamepad, button }
-		} as IButtonEvent);
+	/** Return the state of the specified button */
+	public static getButtonValue(gamepad: number, button: number) {
+		return GamepadHelper.gamepads[gamepad]?.buttons[button]?.value;
 	}
 
-	static createButtonUpEvent(gamepad: number, button: number | string) {
-		GamepadHelper.buttonUpEvent = new CustomEvent("gampadbuttonup", {
-			detail: { gamepad, button }
-		} as IButtonEvent);
+	/** Return the state of the specified axis */
+	public static getAxisValue(gamepad: number, axis: number) {
+		return GamepadHelper.gamepads[gamepad]?.axes[axis];
 	}
 
-	/** Refresh the state of the gamepads connected to the system */
-	static updateGamepads() {
+	/**	Main method intended to be called from within a game loop.
+	 *	Queries the state of each button on each connected gamepad and dispatches an event to the document if there is change in state.
+	 */
+	public static update() {
+		GamepadHelper.checkForNewGamepads();
+
+		// skip if there are no gamepads connected
+		if (!GamepadHelper.gamepads || Object.keys(GamepadHelper.gamepads).length < 1) return;
+
+		if (
+			GamepadHelper.gamepadsLastUpdate &&
+			Object.keys(GamepadHelper.gamepadsLastUpdate).length > 0
+		) {
+			Object.values(GamepadHelper.gamepads).forEach((gamepad) => {
+				// check for changes in button state
+				for (let i = 0; i < gamepad.buttons.length; i++) {
+					const buttonLastUpdate =
+						GamepadHelper.gamepadsLastUpdate[gamepad.index]?.buttons[i];
+					const button = gamepad.buttons[i];
+
+					if (button?.value == 1 && buttonLastUpdate?.value == 0) {
+						GamepadHelper.dispatchButtonDownEvent(gamepad.index, i);
+					} else if (button?.value == 0 && buttonLastUpdate?.value == 1) {
+						GamepadHelper.dispatchButtonUpEvent(gamepad.index, i);
+					}
+				}
+			});
+		}
+
+		// copy to a separate object in order to compare fields next update
+		GamepadHelper.gamepadsLastUpdate = Object.assign({}, GamepadHelper.gamepads);
+	}
+
+	private static checkForNewGamepads() {
+		// @ts-ignore
 		const gamepads = navigator.getGamepads();
 
-		for (let i = 0; i < gamepads.length; i++) {
-			const gamepad = gamepads[i];
+		for (const gamepad of gamepads) {
 			if (gamepad) {
 				if (!GamepadHelper.gamepads[gamepad.index]) {
-					console.log("gamepad " + gamepad.index + " connected");
+					GamepadHelper.logOutput &&
+						console.log("gamepad " + gamepad.index + " connected");
 				}
 				GamepadHelper.gamepads[gamepad.index] = gamepad;
 			}
 		}
 	}
 
-	/** Main method intended to be called from within a game loop */
-	static update() {
-		GamepadHelper.updateGamepads();
-
-		// skip if there are no gamepads connected
-		if (Object.keys(GamepadHelper.gamepads).length < 1) return;
-
-		Object.values(GamepadHelper.gamepads).forEach((gamepad) => {
-			// check for changes in button state
-			for (let i = 0; i < gamepad.buttons.length; i++) {
-				const buttonLastUpdate =
-					GamepadHelper.gamepadsLastUpdate[gamepad.index]?.buttons[i];
-				const button = gamepad.buttons[i];
-				let buttonLabel = i as number | string;
-
-				if (GamepadHelper.mode == "xbox") {
-					buttonLabel = GamepadHelper.getXboxButton(i);
-				} else if (GamepadHelper.mode == "playstation") {
-					buttonLabel = GamepadHelper.getPlaystationButton(i);
-				}
-
-				if (button?.value == 1 && buttonLastUpdate?.value == 0) {
-					console.log(
-						"gamepad " + gamepad.index + ": button " + buttonLabel + " is down"
-					);
-					GamepadHelper.createButtonDownEvent(gamepad.index, buttonLabel);
-					document.dispatchEvent(GamepadHelper.buttonDownEvent);
-				} else if (button?.value == 0 && buttonLastUpdate?.value == 1) {
-					console.log("gamepad " + gamepad.index + ": button " + buttonLabel + " is up");
-					GamepadHelper.createButtonUpEvent(gamepad.index, buttonLabel);
-					document.dispatchEvent(GamepadHelper.buttonUpEvent);
-				}
-			}
-		});
-
-		// copy to a separate object in order to compare fields next update
-		GamepadHelper.gamepadsLastUpdate = Object.assign({}, GamepadHelper.gamepads);
+	private static dispatchButtonDownEvent(gamepad: number, button: number) {
+		GamepadHelper.logOutput && console.log(`gamepad ${gamepad}: button ${button} is down`);
+		document.dispatchEvent(
+			new CustomEvent<IGamepadButtonEventDetail>("gamepadbuttondown", {
+				detail: { gamepad, button }
+			})
+		);
 	}
 
-	/** Return the state of the specified button */
-	static getButtonValue(gamepad: number, button: number) {
-		return GamepadHelper.gamepads[gamepad]?.buttons[button]?.value;
-	}
-
-	/** Return the state of the specified axis */
-	static getAxisValue(gamepad: number, axis: number) {
-		return GamepadHelper.gamepads[gamepad]?.axes[axis];
-	}
-
-	/** Convert to corresponding Xbox button label */
-	static getXboxButton(button: number) {
-		switch (button) {
-			case 0:
-				return "a";
-			case 1:
-				return "b";
-			case 2:
-				return "x";
-			case 3:
-				return "y";
-			case 4:
-				return "lb";
-			case 5:
-				return "rb";
-			case 8:
-				return "back";
-			case 9:
-				return "start";
-			case 10:
-				return "ls";
-			case 11:
-				return "rs";
-			case 12:
-				return "dpad-up";
-			case 13:
-				return "dpad-down";
-			case 14:
-				return "dpad-left";
-			case 15:
-				return "dpad-right";
-			case 16:
-				return "system";
-			default:
-				return button;
-		}
-	}
-
-	/** Convert to corresponding Playstation button label */
-	static getPlaystationButton(button: number) {
-		switch (button) {
-			case 0:
-				return "cross";
-			case 1:
-				return "circle";
-			case 2:
-				return "square";
-			case 3:
-				return "triangle";
-			case 4:
-				return "l1";
-			case 5:
-				return "r1";
-			case 8:
-				return "select";
-			case 9:
-				return "start";
-			case 10:
-				return "l3";
-			case 11:
-				return "r3";
-			case 12:
-				return "dpad-up";
-			case 13:
-				return "dpad-down";
-			case 14:
-				return "dpad-left";
-			case 15:
-				return "dpad-right";
-			case 16:
-				return "system";
-			default:
-				return button;
-		}
-	}
-
-	static setMode(mode: GamepadMode) {
-		GamepadHelper.mode = mode;
+	private static dispatchButtonUpEvent(gamepad: number, button: number) {
+		GamepadHelper.logOutput && console.log(`gamepad ${gamepad}: button ${button} is up`);
+		document.dispatchEvent(
+			new CustomEvent<IGamepadButtonEventDetail>("gamepadbuttonup", {
+				detail: { gamepad, button }
+			})
+		);
 	}
 }
